36db8493f1782b60955a75a2c4c1cd0eacebf103
Artur Bondarenko <ua.bondarenko.artur@gmail.com>
2019-10-05 23:21:08 +0300

    added type

--- parser.gd
+++ parser.gd
@@ -2,8 +2,11 @@ extends Node
 
 enum TYPE {
     location = 1,
-    action = 2
+    action = 2,
+    identification = 3
 }
+
+var client_id = 0
 
 func process_resp(resp):
     var buf = StreamPeerBuffer.new()
@@ -17,3 +20,8 @@ func process_resp(resp):
         print("TYPE: Action")
         while (buf.get_available_bytes() > 0):
             print("X: ", buf.get_16(), " Y: ", buf.get_16(), " Btn: ", buf.get_8())
+    elif msg_type == TYPE.identification:
+        print("TYPE: Identification")
+        client_id = buf.get_8()
+        print("Your id: ", client_id)
+        
